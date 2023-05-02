0c30cbc3230a4adab78d2646782f19942de0b59d
JastinXyz <jastinlinggar@gmail.com>
2023-05-02 18:55:02 +0700

    init

---
+++ README.md
@@ -0,0 +1,68 @@
+# Telegraf Command Handler
+
+A command handler for [Telegraf](https://npmjs.com/telegraf), which is similar to the command handler for Discord.js and such. The goal is that all commands are not in 1 file... But divided into several different files.
+
+## Installation
+
+```bash
+npm i telegraf-command-handler
+# or
+yarn add telegraf-command-handler
+# or
+pnpm add telegraf-command-handler
+```
+
+## Usage
+
+```js
+/* main file */
+const { Telegraf } = require('telegraf');
+const { TelegramCommandHandler } = require('telegraf-command-handler');
+const path = require('path');
+
+const bot = new Telegraf('Your bot token');
+
+const CommandHandler = new TelegramCommandHandler({
+    path: path.resolve() + "/path/to/dir",
+});
+
+bot.use(CommandHandler.load());
+
+// ...bot.launch()
+```
+
+```js
+/* command file */
+module.exports = {
+    name: "start"
+    async execute(ctx) {
+        ctx.reply("hello world");
+    }
+}
+```
+
+## Constructor Options
+
+```ts
+interface Options {
+    /* path to your command directory */
+    path: string;
+    /* a function that executed when error comes from your command file */
+    errorHandling: (ctx: Context, error: any) => any | undefined;
+}
+```
+
+## Command Example
+
+```js
+module.exports = {
+    /* command name (it will executed like /echo) */
+    name: "echo",
+    /* optional command aliases (can be string or Array<string>) */
+    aliases: ["say"],
+    /* command code */
+    async execute(ctx, args) {
+        ctx.reply(args.join(" "))
+    }
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import { Composer, Context } from "telegraf";
+import fs, { Stats } from "fs";
+import path from "node:path";
+
+interface Options {
+  path: string;
+  errorHandling: (ctx: Context, error: any) => any | undefined;
+}
+
+class TelegramCommandHandler {
+  path: string;
+  errorHandling: (ctx: Context, error: any) => any | undefined;
+  commands: Map<string, any>;
+
+  constructor(opts: Options) {
+    if (!opts.path)
+      throw new Error("[telegraf-command-handler] Path required!");
+    if (opts.errorHandling && typeof opts.errorHandling !== "function")
+      throw new Error(
+        '[telegraf-command-handler] "errorHandling" must be a function!'
+      );
+
+    this.path = opts.path;
+    this.errorHandling = opts.errorHandling;
+    this.commands = new Map();
+
+    this.walk(this.path, (x) => {
+      let cmdObj = require(x);
+      this.commands.set(cmdObj.name, cmdObj);
+      console.log(`[telegraf-command-handler] Loaded - ${cmdObj.name}`);
+    });
+  }
+
+  load() {
+    return Composer.on(["edited_message", "message"], async (ctx, next) => {
+      let message = ctx.editedMessage ?? ctx.message as any;
+      let text = message.text ?? message.caption;
+
+      if (message.from.is_bot) return;
+
+      let commands = Array.from(this.commands.values());
+      let args = text.slice(1).split(/\s+/);
+      let command = args.shift().toLowerCase();
+      let commandData = commands.find(
+        (c) =>
+          c.name.toLowerCase() === command.toLowerCase() ||
+          (c.aliases && typeof c.aliases === "object"
+            ? c.aliases.includes(command.toLowerCase())
+            : c.aliases === command.toLowerCase())
+      );
+
+      if (!commandData) return;
+      try {
+        await commandData.execute(ctx, args);
+      } catch (error) {
+        console.error(`Error executing ${command}`);
+        console.error(error);
+        if(this.errorHandling) this.errorHandling(ctx, error);
+      }
+
+      return next();
+    });
+  }
+
+  walk(
+    dir: string,
+    callback: (filepath: string, file: string, stats: Stats) => any
+  ) {
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
+      var filepath = path.join(dir, file);
+      const stats = fs.statSync(filepath);
+      if (stats.isDirectory()) {
+        module.exports.walk(filepath, callback);
+      } else if (stats.isFile()) {
+        callback(filepath, file, stats);
+      }
+    });
+  }
+}
+
+exports.TelegramCommandHandler = TelegramCommandHandler;
